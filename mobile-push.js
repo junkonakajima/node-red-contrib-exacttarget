69e5239a2fbf7a6cdd2a23517b542e5faf0ce167
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2016-01-23 22:48:52 +0900

    Refactor for default values

--- mobile-push.js
+++ mobile-push.js
@@ -1,57 +1,50 @@
 module.exports = function(RED) {
-    var ET_Client = require('fuelsdk-node');
+  var ET_Client = require('fuelsdk-node');
 
-    function MobilePush(config) {
-        RED.nodes.createNode(this,config);
-        var etConfig = RED.nodes.getNode(config.etConfig);
+  function MobilePush(config) {
+    RED.nodes.createNode(this,config);
+    var etConfig = RED.nodes.getNode(config.etConfig);
 
-        this.default = {
-            messageId: config.messageId,
-            deviceTokens: config.deviceTokens,
-            subscriberKeys: config.subscriberKeys,
-            messageText: config.messageText,
-        };
+    var node = this;
+    this.on('input', function(msg) {
+      var client = new ET_Client(etConfig.credentials.clientId, etConfig.credentials.clientSecret, etConfig.credentials.stack);
 
-        var node = this;
-        this.on('input', function(msg) {                
-            var client = new ET_Client(etConfig.credentials.clientId, etConfig.credentials.clientSecret, etConfig.credentials.stack);
+      var body = {};
+      var subscriberKeys = msg.subscriberKeys ? msg.subscriberKeys : config.subscriberKeys.split(',');
+      var deviceTokens =  msg.deviceTokens ? msg.deviceTokens : config.deviceTokens.split(',');
+      if (subscriberKeys.length > 0) {
+        body.SubscriberKeys = subscriberKeys;
+      } else if (deviceTokens.length > 0) {
+        body.DeviceTokens = deviceTokens;
+      }
 
-            var body = {};
-            var subscriberKeys = msg.subscriberKeys ? msg.subscriberKeys : node.default.subscriberKeys.split(',');
-            var deviceTokens =  msg.deviceTokens ? msg.deviceTokens : node.default.deviceTokens.split(',');
-            if (subscriberKeys.length > 0) {
-                body.SubscriberKeys = subscriberKeys;
-            } else if (deviceTokens.length > 0) {
-                body.DeviceTokens = deviceTokens;
-            }
-
-            var messageText = msg.payload ? msg.payload : node.default.messageText;
-            if (messageText && messageText != '') {
-                body.MessageText = messageText;
-                body.Override = true;
-            }
-            var messageId = msg.messageId ? msg.messageId : node.default.messageId;
-            client.RestClient
-                .post({
-                    uri: '/push/v1/messageContact/' + messageId + '/send',
-                    body: body,
-                    json: true
-                })
-                .then(
-                    function(response) {
-                        node.send({
-                            payload: response.body
-                        });
-                    }.bind(this)
-                )
-                .catch(
-                    function(err) {
-                        node.error(err, {
-                            payload: JSON.parse(JSON.stringify(err))
-                        });
-                    }.bind(this)
-                );
-        });
-    }
-    RED.nodes.registerType("mobile-push", MobilePush);
+      var messageText = msg.payload ? msg.payload : config.messageText;
+      if (messageText && messageText != '') {
+        body.MessageText = messageText;
+        body.Override = true;
+      }
+      var messageId = msg.messageId ? msg.messageId : config.messageId;
+      client.RestClient
+        .post({
+          uri: '/push/v1/messageContact/' + messageId + '/send',
+          body: body,
+          json: true
+        })
+        .then(
+          function(response) {
+            node.send({
+              payload: response.body
+            });
+          }.bind(this)
+        )
+        .catch(
+          function(err) {
+            node.error(err, {
+              payload: JSON.parse(JSON.stringify(err))
+            });
+          }.bind(this)
+        );
+    });
+  }
+  RED.nodes.registerType("mobile-push", MobilePush);
 }
--- triggered-send.js
+++ triggered-send.js
@@ -1,82 +1,75 @@
 module.exports = function(RED) {
-    var ET_Client = require('fuelsdk-node');
+  var ET_Client = require('fuelsdk-node');
 
-    function TriggeredSend(config) {
-        RED.nodes.createNode(this,config);
-        var etConfig = RED.nodes.getNode(config.etConfig);
+  function TriggeredSend(config) {
+    RED.nodes.createNode(this,config);
+    var etConfig = RED.nodes.getNode(config.etConfig);
 
-        this.default = {
-            customerKey: config.customerKey,
-            email: config.email,
-            subscriberKey: config.subscriberKey,
-            attributes: config.attributes
-        };
+    var node = this;
+    this.on('input', function(msg) {
+      var createAttributes = function(attributes) {
+        var attributes = attributes ? attributes : {};
+        if (typeof(attributes) === 'string' || attributes instanceof String) {
+          try {
+            attributes = JSON.parse(attributes);
+          } catch (e) {
+            node.error('Attributes is not valid JSON string');
+          }
+        }
+        var returnAttributes = [];
+        for (var key in attributes) {
+          returnAttributes.push({
+            'Name': key,
+            'Value': attributes[key]
+          });
+        }
+        return returnAttributes;
+      };
 
-        var node = this;
-        this.on('input', function(msg) {
-            var createAttributes = function(attributes) {
-                var attributes = attributes ? attributes : {};
-                if (typeof(attributes) === 'string' || attributes instanceof String) {
-                    try {
-                        attributes = JSON.parse(attributes);
-                    } catch (e) {
-                        node.error('Attributes is not valid JSON string');
-                    }
-                }
-                var returnAttributes = [];
-                for (var key in attributes) {
-                    returnAttributes.push({
-                        'Name': key,
-                        'Value': attributes[key]
-                    });
-                }
-                return returnAttributes;
-            };
+      var client = new ET_Client(etConfig.credentials.clientId, etConfig.credentials.clientSecret, etConfig.credentials.stack);        
 
-            var client = new ET_Client(etConfig.credentials.clientId, etConfig.credentials.clientSecret, etConfig.credentials.stack);                
+      var options = {
+        props: {
+          "TriggeredSendDefinition": {
+            "CustomerKey" : msg.customerKey ? msg.customerKey : config.customerKey
+          },
+          "Subscribers": [{
+            "EmailAddress" : msg.email ? msg.email : config.email,
+            "SubscriberKey" : msg.subscriberKey ? msg.subscriberKey : config.subscriberKey,
+            "Attributes" : createAttributes(msg.attributes ? msg.attributes : config.attributes)
+          }]
+        }
+      };
 
-            var options = {
-                props: {
-                    "TriggeredSendDefinition": {
-                        "CustomerKey" : msg.customerKey ? msg.customerKey : node.default.customerKey
-                    },
-                    "Subscribers": [{
-                        "EmailAddress" : msg.email ? msg.email : node.default.email,
-                        "SubscriberKey" : msg.subscriberKey ? msg.subscriberKey : node.default.subscriberKey,
-                        "Attributes" : createAttributes(msg.attributes ? msg.attributes : node.default.attributes)
-                    }]
-                }
-            };
-
-            var subscriber = options.props.Subscribers[0];
-            if (!subscriber.EmailAddress) {
-                var msg = 'EmailAddress is required';
-                node.error(msg, {
-                    payload: msg
-                });
-            }
-            if (!subscriber.SubscriberKey) {
-                var msg = 'SubscriberKey is required';
-                node.error(msg, {
-                    payload: msg
-                });
-            }
-
-            var triggeredSend = client.triggeredSend(options);
-            triggeredSend.send(function(err, response) {
-                if (err) {
-                    node.error(err, {
-                        payload: JSON.parse(JSON.stringify(err))
-                    });
-                } else {
-                    var statusCode = response && response.res && response.res.statusCode ? response.res.statusCode : 200;
-                    var result = response && response.body ? response.body : response;
-                    node.send({
-                        payload: result
-                    });
-                }
-            }); 
+      var subscriber = options.props.Subscribers[0];
+      if (!subscriber.EmailAddress) {
+        var msg = 'EmailAddress is required';
+        node.error(msg, {
+          payload: msg
         });
-    }
-    RED.nodes.registerType("triggered-send", TriggeredSend);
+      }
+      if (!subscriber.SubscriberKey) {
+        var msg = 'SubscriberKey is required';
+        node.error(msg, {
+          payload: msg
+        });
+      }
+
+      var triggeredSend = client.triggeredSend(options);
+      triggeredSend.send(function(err, response) {
+        if (err) {
+          node.error(err, {
+            payload: JSON.parse(JSON.stringify(err))
+          });
+        } else {
+          var statusCode = response && response.res && response.res.statusCode ? response.res.statusCode : 200;
+          var result = response && response.body ? response.body : response;
+          node.send({
+            payload: result
+          });
+        }
+      }); 
+    });
+  }
+  RED.nodes.registerType("triggered-send", TriggeredSend);
 }
