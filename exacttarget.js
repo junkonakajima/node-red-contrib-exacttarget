85ea065d65e67692ee5881abce3a47b915e424db
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2016-01-24 15:51:08 +0900

    Mod indent from 4 spaces to 2 spaces

--- exacttarget.js
+++ exacttarget.js
@@ -1,20 +1,20 @@
 module.exports = function(RED) {
-    function ExactTargetConfig(n) {
-        RED.nodes.createNode(this,n);
-        this.name = n.name;
-        this.clientId = n.clientId;
-        this.clientSecret = n.clientSecret;
-        this.stack = n.stack;
+  function ExactTargetConfig(n) {
+    RED.nodes.createNode(this,n);
+    this.name = n.name;
+    this.clientId = n.clientId;
+    this.clientSecret = n.clientSecret;
+    this.stack = n.stack;
+  }
+  RED.nodes.registerType(
+    "exacttarget", 
+    ExactTargetConfig,
+    {
+      credentials: {
+        clientId: {type: "text"},
+        clientSecret: {type: "password"},
+        stack: {type: "text"}
+      }
     }
-    RED.nodes.registerType(
-        "exacttarget", 
-        ExactTargetConfig,
-        {
-            credentials: {
-                clientId: {type: "text"},
-                clientSecret: {type: "password"},
-                stack: {type: "text"}
-            }
-        }
-    );
+  );
 }
